328dddf6a96a7d87f48814c2d3fdfc2386ad8b14
Yuliya Levina <rusalka617@gmail.com>
2023-10-14 19:35:31 +0300

    second commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,13 +1,12 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    void shouldCalculateMissingQty() {
+    public void shouldCalculateMissingQty() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int expected = 1;
@@ -16,7 +15,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    void amountEqualBoundary() {
+    public void amountEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
@@ -25,7 +24,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    void amountMoreBoundary() {
+    public void amountMoreBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int expected = 999;
